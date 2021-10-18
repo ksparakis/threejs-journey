1917dbee529a66f9d772d8e795a78ed79c099ffa
ksparakis <ksparakis@gmail.com>
2021-10-18 01:20:09 -0400

    finished class 7 added class 8

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -1,5 +1,25 @@
 import './style.css'
 import * as THREE from 'three'
+import  { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// Sizes
+const sizes = {
+    width: 800,
+    height: 600
+}
+/**
+ *  Cursor
+ */
+
+const cursor = {
+    x:0,
+    y:0
+}
+window.addEventListener('mousemove', (event) => {
+    // lets us have a 0 in the middle of the screen
+    cursor.x = event.clientX / sizes.width - 0.5;
+    cursor.y = - (event.clientY / sizes.height -0.5);
+})
 
 /**
  * Base
@@ -7,27 +27,31 @@ import * as THREE from 'three'
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
-// Sizes
-const sizes = {
-    width: 800,
-    height: 600
-}
-
 // Scene
 const scene = new THREE.Scene()
 
 // Object
 const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.BoxGeometry(.5, .5, .5, 5, 5, 5),
     new THREE.MeshBasicMaterial({ color: 0xff0000 })
 )
 scene.add(mesh)
 
+mesh.position.z =2
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
+const aspectRatio = sizes.width /sizes.height
+const camera = new THREE.OrthographicCamera(
+     -1 * aspectRatio,
+    1 ,
+    1,
+    -1,
+    0.1,
+    200
+    )
+//camera.position.x = 2
+//camera.position.y = 2
+camera.position.z = 3
 camera.lookAt(mesh.position)
 scene.add(camera)
 
@@ -40,12 +64,21 @@ renderer.setSize(sizes.width, sizes.height)
 // Animate
 const clock = new THREE.Clock()
 
+//Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
 
     // Update objects
-    mesh.rotation.y = elapsedTime;
+    // camera.position.x = Math.sin(cursor.x * Math.PI * 2) * 3
+    // camera.position.z = Math.cos(cursor.x * Math.PI * 2) * 3
+    // camera.position.y = cursor.y * 5
+    // camera.lookAt(mesh.position)
+
+    // Using Controls instead
+    controls.update()
 
     // Render
     renderer.render(scene, camera)
