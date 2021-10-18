f2361259134dfc26bcffb1d0b7a4be0a76c5ec07
ksparakis <ksparakis@gmail.com>
2021-10-18 01:48:44 -0400

    finished lesson 8

--- 07-cameras/src/script.js
+++ 07-cameras/src/script.js
@@ -60,7 +60,7 @@ const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
 renderer.setSize(sizes.width, sizes.height)
-
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 // Animate
 const clock = new THREE.Clock()
 
